e9bab9d117c565fad93584dbc15ec029617a9d07
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2020-08-22 04:00:08 +0300

    deploy test

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { UserContextProvider } from "./Context/userContext";
 
 ReactDOM.render(
     <UserContextProvider>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </UserContextProvider>,
     document.getElementById("root")
 );
