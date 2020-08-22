e959ea82cb09409a52c501366779f7e162a666b7
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2020-08-22 03:54:40 +0300

    Updates

---
+++ precache-manifest.f26b2ae20603ee6b0626b8200328edfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4bd272f04b2b6057f7ee85312d2ff7",
+    "url": "/react-invoice/index.html"
+  },
+  {
+    "revision": "be593e1ab34f69044065",
+    "url": "/react-invoice/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "ead63cf5df3fb37a0ab0",
+    "url": "/react-invoice/static/css/main.4ca73074.chunk.css"
+  },
+  {
+    "revision": "be593e1ab34f69044065",
+    "url": "/react-invoice/static/js/2.69ee5d3a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-invoice/static/js/2.69ee5d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ead63cf5df3fb37a0ab0",
+    "url": "/react-invoice/static/js/main.8aa9339e.chunk.js"
+  },
+  {
+    "revision": "9b2da40acaa32b1b7488",
+    "url": "/react-invoice/static/js/runtime-main.58295419.js"
+  }
+]);
